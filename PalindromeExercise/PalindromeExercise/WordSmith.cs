1b7a6e1695c0ba49c74efab4d7428e1f507e717d
Garrett Chitwood <gman@acvwa.com>
2025-10-26 17:31:38 -0700

    Created Wordsmith class, and setup the UnitTest method. Struggling with my var setup however.

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PalindromeExercise;
+
+public class WordSmith
+{
+    public bool IsAPalindrome(string text)
+    {
+        throw new NotImplementedException();
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -5,10 +5,25 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)] // ← you will need to put some test data and expected result here.
+        // For example [InlineData(“racecar”, true)]
+        [InlineData("dad", true)]
+        [InlineData("mom", true)]
+        [InlineData("bob", true)]
+        [InlineData("keyfob", false)]
+        [InlineData("boat", false)]
+        [InlineData("red", false)]
+        [InlineData("star", false)]
+        public void Test1(string text, bool expected)
         {
-
+            //Arrange
+            var wordtest = new Palindrome();
+            
+            //Act
+            
+            //Assert
+            Assert.Equal;
         }
     }
 }
